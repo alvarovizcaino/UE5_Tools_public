c5e57808f4007094ea05e7f06b0046b681dda782
Alvaro Vizcaino <a.vizcaino.dejulian@gmail.com>
2022-09-14 21:05:54 +0200

    Añadida Tool para renombrar archivos en Python

---
+++ Source/Tools_Project/TL_AssetActionUtility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetActionUtility.h"
+#include "TL_AssetActionUtility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOOLS_PROJECT_API UTL_AssetActionUtility : public UAssetActionUtility
+{
+	GENERATED_BODY()
+	
+};
