30ed260b7870553c89d1c76b7c897950e171910f
Alvaro Vizcaino <a.vizcaino.dejulian@gmail.com>
2022-09-14 22:02:31 +0200

    Added C++ Tool to change asset names

--- Source/Tools_Project/TL_AssetActionUtility.cpp
+++ Source/Tools_Project/TL_AssetActionUtility.cpp
@@ -2,4 +2,55 @@
 
 
 #include "TL_AssetActionUtility.h"
+#include "EditorUtilityLibrary.h"
 
+void UTL_AssetActionUtility::RenameSelectedAssets(FString SearchPattern, FString ReplacePattern, ESearchCase::Type SearchCase)
+{
+    // Check if something needs to be done
+    if (SearchPattern.IsEmpty() || SearchPattern.Equals(ReplacePattern, SearchCase))
+    {
+        return;
+    }
+
+    // Get Selected objects:
+    TArray<UObject*> SelectedObjects = UEditorUtilityLibrary::GetSelectedAssets();
+    uint32 Counter = 0;
+
+    // Check each Asset if it needs to be renamed
+    for (UObject* SelectedObject : SelectedObjects)
+    {
+        if (ensure(SelectedObject)) // Otra manera de asegurarnos de que no es puntero nulo.
+        {
+            FString AssetName = SelectedObject->GetName();
+            if (AssetName.Contains(SearchPattern, SearchCase))
+            {
+                FString NewName = AssetName.Replace(*SearchPattern, *ReplacePattern, SearchCase);
+                UEditorUtilityLibrary::RenameAsset(SelectedObject, NewName);
+                ++Counter;
+            }
+        }
+    }
+
+    GiveFeedback(TEXT("Renamed"), Counter);
+}
+
+void UTL_AssetActionUtility::PrintToScreen(FString Message, FColor Color)
+{
+    if (ensure(GEngine))
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 2.5f, Color, Message);
+    }
+}
+
+void UTL_AssetActionUtility::GiveFeedback(FString Method, uint32 Counter)
+{
+    FString Message = FString("No matching files found");
+    FColor Color = Counter > 0 ? FColor::Green : FColor::Red;
+    if (Counter > 0)
+    {
+        Message = Method.AppendChar(' ');
+        Message.AppendInt(Counter);
+        Message.Append(Counter == 1 ? TEXT(" file") : TEXT(" files"));
+    }
+    PrintToScreen(Message, Color);
+}
--- Source/Tools_Project/TL_AssetActionUtility.h
+++ Source/Tools_Project/TL_AssetActionUtility.h
@@ -14,4 +14,31 @@ class TOOLS_PROJECT_API UTL_AssetActionUtility : public UAssetActionUtility
 {
 	GENERATED_BODY()
 	
+public:
+	/**
+	 * Rename the selected assets
+	 * @param SearchPattern - the pattern to search for
+	 * @param ReplacePattern - the pattern to replace with
+	 * @param SearchCase - should the case be ignored?
+	 */
+
+	UFUNCTION(CallInEditor)
+	void RenameSelectedAssets(FString SearchPattern, FString ReplacePattern, ESearchCase::Type SearchCase);
+
+private:
+
+	/**
+	 * Prints a message to the screen
+	 * @param Message - the message to print
+	 * @param Color - the Color to use for the Message
+	 */
+	 void PrintToScreen(FString Message, FColor Color);
+
+	 /**
+	  * Give Feedback to the user
+	  * @param Method - the Method that was used
+	  * @param Counter - how many objects were affected
+	  */
+	  void GiveFeedback(FString Method, uint32 Counter);
+
 };
